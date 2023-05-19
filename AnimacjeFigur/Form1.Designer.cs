a6e62f481630a4f7a4c45ddd087f300db1f34b65
SebastianSzt <94396040+SebastianSzt@users.noreply.github.com>
2023-05-19 21:17:29 +0200

    Zajecia 15.05.2023

--- AnimacjeFigur/Form1.Designer.cs
+++ AnimacjeFigur/Form1.Designer.cs
@@ -28,109 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            menuStrip1 = new MenuStrip();
-            plikToolStripMenuItem = new ToolStripMenuItem();
-            wczytajToolStripMenuItem = new ToolStripMenuItem();
-            zapiszToolStripMenuItem = new ToolStripMenuItem();
-            animacjaToolStripMenuItem = new ToolStripMenuItem();
-            uruchomToolStripMenuItem = new ToolStripMenuItem();
-            zatrzymajToolStripMenuItem = new ToolStripMenuItem();
-            panel1 = new Panel();
-            timer1 = new System.Windows.Forms.Timer(components);
-            menuStrip1.SuspendLayout();
-            SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            menuStrip1.Items.AddRange(new ToolStripItem[] { plikToolStripMenuItem, animacjaToolStripMenuItem });
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(784, 24);
-            menuStrip1.TabIndex = 0;
-            menuStrip1.Text = "menuStrip1";
-            // 
-            // plikToolStripMenuItem
-            // 
-            plikToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { wczytajToolStripMenuItem, zapiszToolStripMenuItem });
-            plikToolStripMenuItem.Name = "plikToolStripMenuItem";
-            plikToolStripMenuItem.Size = new Size(38, 20);
-            plikToolStripMenuItem.Text = "Plik";
-            // 
-            // wczytajToolStripMenuItem
-            // 
-            wczytajToolStripMenuItem.Enabled = false;
-            wczytajToolStripMenuItem.Name = "wczytajToolStripMenuItem";
-            wczytajToolStripMenuItem.Size = new Size(115, 22);
-            wczytajToolStripMenuItem.Text = "Wczytaj";
-            wczytajToolStripMenuItem.Click += loadStatus;
-            // 
-            // zapiszToolStripMenuItem
-            // 
-            zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
-            zapiszToolStripMenuItem.Size = new Size(115, 22);
-            zapiszToolStripMenuItem.Text = "Zapisz";
-            zapiszToolStripMenuItem.Click += saveStatus;
-            // 
-            // animacjaToolStripMenuItem
-            // 
-            animacjaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { uruchomToolStripMenuItem, zatrzymajToolStripMenuItem });
-            animacjaToolStripMenuItem.Name = "animacjaToolStripMenuItem";
-            animacjaToolStripMenuItem.Size = new Size(69, 20);
-            animacjaToolStripMenuItem.Text = "Animacja";
-            // 
-            // uruchomToolStripMenuItem
-            // 
-            uruchomToolStripMenuItem.Name = "uruchomToolStripMenuItem";
-            uruchomToolStripMenuItem.Size = new Size(126, 22);
-            uruchomToolStripMenuItem.Text = "Uruchom";
-            uruchomToolStripMenuItem.Click += timerStart;
-            // 
-            // zatrzymajToolStripMenuItem
-            // 
-            zatrzymajToolStripMenuItem.Enabled = false;
-            zatrzymajToolStripMenuItem.Name = "zatrzymajToolStripMenuItem";
-            zatrzymajToolStripMenuItem.Size = new Size(126, 22);
-            zatrzymajToolStripMenuItem.Text = "Zatrzymaj";
-            zatrzymajToolStripMenuItem.Click += timerStop;
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wczytajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.animacjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uruchomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zatrzymajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
             // 
             // panel1
             // 
-            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            panel1.BackColor = SystemColors.ActiveCaptionText;
-            panel1.Location = new Point(0, 27);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(784, 384);
-            panel1.TabIndex = 1;
-            panel1.Paint += panel1_Paint;
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 384);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.animacjaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wczytajToolStripMenuItem,
+            this.zapiszToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // wczytajToolStripMenuItem
+            // 
+            this.wczytajToolStripMenuItem.Enabled = false;
+            this.wczytajToolStripMenuItem.Name = "wczytajToolStripMenuItem";
+            this.wczytajToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.wczytajToolStripMenuItem.Text = "Wczytaj";
+            this.wczytajToolStripMenuItem.Click += new System.EventHandler(this.loadStatus);
+            // 
+            // zapiszToolStripMenuItem
+            // 
+            this.zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
+            this.zapiszToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.zapiszToolStripMenuItem.Text = "Zapisz";
+            this.zapiszToolStripMenuItem.Click += new System.EventHandler(this.saveStatus);
+            // 
+            // animacjaToolStripMenuItem
+            // 
+            this.animacjaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uruchomToolStripMenuItem,
+            this.zatrzymajToolStripMenuItem});
+            this.animacjaToolStripMenuItem.Name = "animacjaToolStripMenuItem";
+            this.animacjaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.animacjaToolStripMenuItem.Text = "Animacja";
+            // 
+            // uruchomToolStripMenuItem
+            // 
+            this.uruchomToolStripMenuItem.Name = "uruchomToolStripMenuItem";
+            this.uruchomToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.uruchomToolStripMenuItem.Text = "Uruchom";
+            this.uruchomToolStripMenuItem.Click += new System.EventHandler(this.timerStart);
+            // 
+            // zatrzymajToolStripMenuItem
+            // 
+            this.zatrzymajToolStripMenuItem.Enabled = false;
+            this.zatrzymajToolStripMenuItem.Name = "zatrzymajToolStripMenuItem";
+            this.zatrzymajToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.zatrzymajToolStripMenuItem.Text = "Zatrzymaj";
+            this.zatrzymajToolStripMenuItem.Click += new System.EventHandler(this.timerStop);
             // 
             // timer1
             // 
-            timer1.Interval = 50;
-            timer1.Tick += timer1_Tick;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(784, 411);
-            Controls.Add(panel1);
-            Controls.Add(menuStrip1);
-            DoubleBuffered = true;
-            MainMenuStrip = menuStrip1;
-            MinimumSize = new Size(200, 200);
-            Name = "Form1";
-            StartPosition = FormStartPosition.CenterParent;
-            Text = "Animacje Figur";
-            ResizeEnd += checkPosition;
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Name = "Form1";
+            this.Text = "Animacje Figur";
+            this.ResizeEnd += new System.EventHandler(this.checkPosition);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
 
+        private Panel panel1;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem plikToolStripMenuItem;
         private ToolStripMenuItem wczytajToolStripMenuItem;
@@ -138,7 +147,6 @@
         private ToolStripMenuItem animacjaToolStripMenuItem;
         private ToolStripMenuItem uruchomToolStripMenuItem;
         private ToolStripMenuItem zatrzymajToolStripMenuItem;
-        private Panel panel1;
         private System.Windows.Forms.Timer timer1;
     }
 }
